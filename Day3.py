a84046d8c78aedc1cb846ea727e4c2be124c1d69
mounika251 <74923257+mounika251@users.noreply.github.com>
2020-11-25 16:41:32 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -18,11 +18,11 @@ b=[2,3,4]
 print("the value of a ",a) 
 print("the value of b ",b) 
 temp={}
-for i in a:
-  for j in b:
+For i in a:
+  For j in b:
     temp[i]=j
     b.remove(j)
-  break
+  Break
 print("after mapping two lists in a dictionary ",temp)
 
 #4) write a python program to find the length of a set
