bd36e9d6fec673bb311b311fb70a0b5c0954ba98
mounika251 <74923257+mounika251@users.noreply.github.com>
2020-11-25 15:59:55 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,21 @@
+#1) write a python script to merge two python dictionaries
+
+x={"a":1,"b":2}
+y={"c":3,"d":4}
+x. update(y)
+print(x)
+
+#2) write a python program to remove a key from dictionary 
+
+fruits={"apple":2,"banana":3,"orange":4}
+fruits.pop("banana")
+print(fruits)
+
+#3)write a python program to two lists into a dictionary 
+
+a={"apple","banana","orange"}
+b={2,3,4}
+print("the value of a ",a) 
+print("the value of b ",b) 
+temp={}
+
