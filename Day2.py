1a5fb88b8c2bae6c8cf8e0fa80e72e847a829f2f
mounika251 <74923257+mounika251@users.noreply.github.com>
2020-11-25 12:09:24 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -7,7 +7,7 @@ a=b=c=40
 div=int(a/10)
 mul=int(b*50)
 add=int(c+60)
-Print("divided a by 10 =",div)
+print("divided a by 10 =",div)
 print("multiply b by 50 =",mul) 
 print("add c by 60 =",add) 
 
@@ -15,7 +15,7 @@ print("add c by 60 =",add)
 
 str="madam"
 rep=str.replace("d","g")
-Print("replace 3 rd character with g is",replace) 
+print("replace 3 rd character with g is",replace) 
 
 #3.create 2 values (a, b) of int, float data type and convert the vice versa 
 
@@ -25,7 +25,7 @@ a=int(a)
 b=float(b)
 c=float(a)
 d=int(b)
-Print('before converting the value of a is in int data type ',a) 
+print('before converting the value of a is in int data type ',a) 
 print('converting the value a from int to float ',c) 
 print('before converting the value of b is in float data type ',b) 
 print('converting the value of b from float to int ',d) 
