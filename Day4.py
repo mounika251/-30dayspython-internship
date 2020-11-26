7f8cde573df4bcc304a22f4d06739c72c67f6271
mounika251 <74923257+mounika251@users.noreply.github.com>
2020-11-26 18:04:40 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,58 +1,58 @@
 #1) write a program to create a list in of n integer values and do the following
-a.add an item in to the list(using funtion)
+# i) dd an item in to the list(using funtion)
 
-using  append() method
+# using  append() method
 
 a=["apple","banana","mango"]
 print("before adding an item in to the list",a)
 a.append("cherry")
 print("After adding an item in to the list",a)
 
-using insert() method
+#using insert() method
 
 a=["apple","banana","mango"]
 a.insert(2,"orange")
 print(a)
 
-b.delete(using function)
+# ii) delete(using function)
 
-using remove() method
+# using remove() method
 
 l1=[1,2,3]
 l1.remove("2")
 print(l1)
 
-using pop() methood
+# using pop() methood
 
 l1=[1,2,3,4]
 l1.pop()
 print(l1)
 
-using clear() method
+# using clear() method
 l1=[1,2,3,4,5]
 l1.clear()
 print(l1)
 
-c.store the largest number from the list to a variable
+# iii) store the largest number from the list to a variable
 
 a=[1,2,3,10,7,8,20]
 l=max(a)
 print(l)
 
-d.store the smallest number from the list to a variable
+# iv) store the smallest number from the list to a variable
 
 l1=[200,800,100,600,700]
 l=min(l1)
 print(l)
 
-2.create a tuple and print the reverse of the created tuple
+# 2.create a tuple and print the reverse of the created tuple
 
 t1=(1,2,3,4,5)
 print(t1)
 rev=reversed(t1)
 print(tuple(rev))
 
-3.create a tuple and convert tuples into list
+# 3.create a tuple and convert tuples into list
 
 tuple1=(1,2,3,4,5)
 print("list of items in tuple 1",tuple1)
