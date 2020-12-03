725737617b0f3ce5409c6861bd65d5d4920b0cb9
mounika251 <74923257+mounika251@users.noreply.github.com>
2020-12-03 13:40:17 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -1,3 +1,4 @@
+#1)list down all the error types and check all the errors using a python program for all errors 
 print("types of errors")
 print("1.index error")
 a=[1,2,3]
@@ -46,5 +47,42 @@ try:
 except:
     print("null pointer exception")
 
-
-
+#2)design a simple calculator app with try and except for all use cases
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+try:
+    while True:
+        choice = input("Enter choice(1/2/3/4): ")
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+                break
+            else:
+                print("Invalid Input")
+except Exception as e:
+    print(e)
+    
+#3)print one message if the try block raises a name error and another for other errors
+ try:
+        print(l)
+ except NameError:
+    print("variable is not defined")
